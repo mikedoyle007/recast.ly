ee864a9ec73e5fe6d85d7f88a5b98816a9461ff4
Mike Doyle <mikedoyle007@gmail.com>
2017-07-12 19:27:41 -0700

    View thumbnails

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,7 +2,7 @@ var VideoList = (props) => (
   <div className="video-list media">
     {
       props.videos.map( video => 
-        <VideoListEntry videos={video} />
+        <VideoListEntry video={video} />
       )
     }
   </div>
